ad2336d514ab2aa0d9926750a7c077ba8798b5de
Sean P. Goggins <s@goggins.com>
2025-08-06 14:17:59 -0500

    updates
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- 8knot_multi_host/generator_archive/test.sh
+++
@@ -1,1 +0,0 @@
- podman-compose down --remove-orphans && podman container prune -f && sudo systemctl reset-failed && sudo find /run/systemd/transient /etc/systemd/system -name 'podman-*.timer' -delete
