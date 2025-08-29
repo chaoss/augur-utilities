86a1eb07d230e395b3eebd3e7513f5e3185c8148
Adrian Edwards <adredwar@redhat.com>
2025-08-29 17:15:26 -0400

    adjust package structure

--- email_hasher/hash-augur-email.py
+++ email_hasher/hash-augur-email.py
@@ -13,7 +13,7 @@
 """
 # -- Enable the pgcrypto extension if not already enabled.
 import sys
-from email_hasher.helpers import encrypt_emails, read_db_config, connect_to_db
+from db_tools import encrypt_emails, read_db_config, connect_to_db
 
 def main(secret_key):
     db_config = read_db_config()
